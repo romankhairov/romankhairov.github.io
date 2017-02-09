f56697b8e8508e13d6d5d2ca36d2e76e84bf3e6b
Roman Khairov <roman.khairov@gmail.com>
2017-02-09 20:49:24 +0100

    Fix bugs with selection. Add loop for photos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-var btn_prev = document
+var btn_prev = document.querySelector('#gallery .buttons .prev');
+var btn_next = document.querySelector('#gallery .buttons .next');
+
+var images = document.querySelectorAll('#gallery .photos img');
+var i = 0;
+
+btn_prev.onclick = function() {
+  alert(1);
+}
+
+btn_next.onclick = function() {
+  images[i].style.display = 'none';
+  i ++;
+
+  if(i >= images.length) {
+    i = 0;  
+  }
+
+  images[i].style.display = 'block';
+}
