64a8d61b0f42ab30494e6d26f6d70fcb2bfa72fc
Roman Khairov <roman.khairov@gmail.com>
2017-07-26 22:41:11 +0200

    Add description of features of the app

---
+++ DailyBudgetCalculator/app.js
@@ -0,0 +1,11 @@
+// Features of application:
+// 1) Summ all income money. Possibility to add more input fields.
+// 2) Summ all monthly payments and minus from total 15% for savings
+// 3) Split the rest of the money to ammount days in picked month
+// 4) Possibility to add expenses to picked day. After that daily summ
+// for the rest days recalculates.
+
+// 1) input
+
+var monthlySalary = document.querySelector('.salary').value;
+console.log(monthlySalary);
