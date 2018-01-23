b644d23884d17da2dd880764378d188b4851a2ec
Roman Khairov <roman.khairov@gmail.com>
2018-01-23 21:59:14 +0100

    Add input field with adding li and close button

---
+++ DailyBudgetCalculator/resources/js/app.js
@@ -0,0 +1,58 @@
+// Features of application:
+// 1)two main blocks: input (salary, rent, savings) and output (calculated daily budget)
+// 2)in the end is summarise- how much I saved
+// 3)income block has inputs of name and value
+// 4)output block is a table, rows depends from days in selected month
+// 5)app use local Storage
+
+// writing base input blocks
+
+
+
+// ****************** Input Block ***************************
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
