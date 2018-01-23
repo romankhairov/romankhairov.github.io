870e8e058220df499d6d5cbf5bf96356df03eabe
Roman Khairov <roman.khairov@gmail.com>
2018-01-23 22:14:07 +0100

    Add values to array

--- DailyBudgetCalculator/resources/js/app.js
+++ DailyBudgetCalculator/resources/js/app.js
@@ -11,48 +11,32 @@
 
 // ****************** Input Block ***************************
 // Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
+var theList = [];
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+  function inputList() {
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-}
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+    var item = document.getElementById('todoInput').value;
+    var text = document.createTextNode(item);
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.name = "name";
+    checkbox.value = "value";
+    var newItem = document.createElement("li");
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+    newItem.appendChild(checkbox);
+    newItem.appendChild(text);
+    document.getElementById("inputList").appendChild(newItem);
+
+    theList.push(item); // This adds the item to theList[]
+    //localStorage.setItem("inputList", JSON.stringify(theList)); // Set localStorage object
+    //var storedTodoList = JSON.parse(localStorage.getItem("inputList")); // Get localStorage object
+
+    console.log(theList);
+
+
+    return clear();
   }
-}
+
+  function clear() {
+    todoInput.value = "";
+  }
