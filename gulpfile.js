c3d170ea29f916d1eb2144284b83f3b02e3f4b4d
Roman <roman.khairov@gmail.com>
2018-02-04 21:03:51 +0100

    setup automatisation for my project

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars');
+
+gulp.task('default', function() {
+  console.log("Yo, you made Gulp task");
+});
+
+gulp.task('html', function() {
+  console.log("Imagine something useful being done to your HTML here.");
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./assets/styles/styles.css')
+    .pipe(postcss([cssvars, autoprefixer]))
+    .pipe(gulp.dest('./assets/temp/styles'));
+});
+
+gulp.task('watch', function() {
+
+  watch('./index.html', function() {
+    gulp.start('html');
+  });
+
+  watch('./assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  });
+
+});
