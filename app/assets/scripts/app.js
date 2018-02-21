916c9066775bca32fb9af91b85e26d44c5327e90
Roman <roman.khairov@gmail.com>
2018-02-21 21:08:47 +0100

    update title and fix primary nav

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
 import Modal from './modules/Modal';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
 var modal = new Modal();
+var stickyHeader = new StickyHeader();
